46905036944c9635e8cb83410fe2646764cb2fc4
sangeethsangii <sangeethk710@gmail.com>
2024-09-02 11:37:23 +0530

    first commit

---
+++ custom/sale_customization/__manifest__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': 'Sale Customization',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Customizations for sales orders',
+    'description': 'Adds a field to mark products as quotation only and prevents conversion if such products are present.',
+    'depends': ['sale','product'],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
+
+
+
+
+
+
+
+
---
+++ custom/sale_customization/models/sale_order.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, exceptions
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        for order in self:
+            # Search for order lines with products marked as 'is_quotation_only_product'
+            quotation_lines = self.env['sale.order.line'].search([
+                ('order_id', '=', order.id),
+                ('product_id.is_quotation_only_product', '=', True)
+            ])
+            if quotation_lines:
+                raise exceptions.UserError(
+                    'You cannot confirm a quotation because it contains quotation only products.'
+                )
+        return super(SaleOrder, self).action_confirm()
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.template'
+
+    is_quotation_only_product = fields.Boolean(
+        string='Quotation Only Product',
+        help='If checked, this product can only be used in quotations and cannot be sold.'
+    )
+
+
+
