53f7ea1eb2cd4d944cacc15c0967a27a545a2294
sangeethsangii <sangeethk710@gmail.com>
2024-11-06 17:39:34 +0530

    first commit

---
+++ dev/project_custom_stage/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Project Custom Default Stage',
+    'version': '1.0',
+    'depends': ['project'],
+    'data': [
+        'views/project_task_stage_view.xml',
+    ],
+}
---
+++ dev/project_custom_stage/models/project_task_stage.py
@@ -0,0 +1,35 @@
+from odoo import fields, models, api
+
+
+class ProjectTaskType(models.Model):
+    _inherit = 'project.task.type'
+
+    is_default_stage = fields.Boolean(string="Default Stage")
+
+
+class Project(models.Model):
+    _inherit = 'project.project'
+    _description = 'Project'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """
+        Override create method to automatically create and assign default stages
+        for new projects.
+        """
+        projects = super().create(vals_list)
+        default_stage_ids = self.env['project.task.type'].search([('is_default_stage', '=', True)])
+        print(default_stage_ids)
+        for project in projects:
+            for default_stage_id in default_stage_ids:
+                print(default_stage_id.project_ids)
+                default_stage_id.project_ids += project
+        return projects
+
+
+
+
+
+
+
+
